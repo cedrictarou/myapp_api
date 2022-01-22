45aa8978ea885118c795a5bbf16c7066f5c2d6a0
cedrictaro-g <cedric.tarou@gmail.com>
2022-01-23 07:19:52 +0900

    Add authenticate service module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
+  before_action :authenticate_active_user
+
   def index
-    users = User.all
-    render json: users.as_json(only: %i[id name email created_at])
+    # users = User.all
+    render json: current_user.as_json(only: %i[id name email created_at])
   end
 end
